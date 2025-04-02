e0f61088e7da716791265e9802f394e5f053f4d6
허서영 <hsy3130@outlook.kr>
2025-04-02 22:23:13 +0900

    create-spring-boot-app

---
+++ src/test/java/com/moonbaar/MoonbaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonbaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonbaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
