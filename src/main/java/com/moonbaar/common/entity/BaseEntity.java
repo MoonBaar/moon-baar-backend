53e9387665158bfc8bacd36087feee737bee4ef9
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-03 12:04:00 +0900

    chore: 프로젝트 기본 구조 설정 #2
    - BaseEntity 구현
    - BusinessException 구현
    - GlobalExceptionHandler 구현
    - ErrorResponse 클래스 구현
    - 기본 에러 코드 정의
    - 데이터 유효성 검증을 위한 spring-boot-starter-validation 의존성 추가

---
+++ src/main/java/com/moonbaar/common/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.moonbaar.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @NotNull
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/moonbaar/common/entity/BaseEntityWithUpdate.java
@@ -0,0 +1,21 @@
+package com.moonbaar.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntityWithUpdate extends BaseEntity {
+
+    @NotNull
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
