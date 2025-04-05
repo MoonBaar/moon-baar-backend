a69fbd896aeaa3fe61d151651c009e6e2f3df2fb
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-05 21:45:36 +0900

    feat: 문화행사 관련 엔티티 구현
    - Category, District, CulturalEvent 엔티티 클래스 구현
    - ERD DB 스키마와 일치하도록 설정

---
+++ src/main/java/com/moonbaar/domain/category/entity/Category.java
@@ -0,0 +1,31 @@
+package com.moonbaar.domain.category.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String name;
+}
