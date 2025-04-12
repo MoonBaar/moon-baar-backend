2e66ff54fd3aa39df150e2cfc111cd4c28da0b58
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-13 01:32:55 +0900

    feat: 좋아요 취소 엔드포인트 구현

--- src/main/java/com/moonbaar/domain/like/controller/LikeController.java
+++ src/main/java/com/moonbaar/domain/like/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.moonbaar.domain.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LikeController {
 
     // 임시 유저 아이디
-    private Long userId = 1L;
+    private Long MOCK_USER_ID = 1L;
 
     private final LikeService likeService;
 
     @PostMapping("/{eventId}/like")
     public ResponseEntity<LikeResponse> likeEvent(@PathVariable Long eventId) {
-        LikeResponse response = likeService.likeEvent(eventId, userId);
+        LikeResponse response = likeService.likeEvent(eventId, MOCK_USER_ID);
         return ResponseEntity.ok(response);
     }
 
+    @DeleteMapping("/{eventId}/like")
+    public ResponseEntity<LikeResponse> unlikeEvent(@PathVariable Long eventId) {
+        LikeResponse response = likeService.unlikeEvent(MOCK_USER_ID, eventId);
+        return ResponseEntity.ok(response);
+    }
 }
