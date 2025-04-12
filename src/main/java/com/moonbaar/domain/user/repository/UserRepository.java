daab055e3247e79d821e123eb7a7795765c7d1b3
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-13 01:23:38 +0900

    feat: 좋아요 서비스에 필요한 User 로직 구현

---
+++ src/main/java/com/moonbaar/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.moonbaar.domain.user.repository;
+
+import com.moonbaar.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
