416884e1646d8d479841cde65370a25c698d2dcc
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-13 00:20:56 +0900

    feat: LikedEvent 엔티티 구현
    - ERD에 맞게 LikedEvent entity 설정

---
+++ src/main/java/com/moonbaar/domain/like/entity/LikedEvent.java
@@ -0,0 +1,32 @@
+package com.moonbaar.domain.like.entity;
+
+import com.moonbaar.common.entity.BaseEntity;
+import com.moonbaar.domain.event.entity.CulturalEvent;
+import com.moonbaar.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "liked_events")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class LikedEvent extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private CulturalEvent event;
+}
