b69d42ff825fe6b3da5222198f3302480e2cfe8d
허서영 <hsy3130@outlook.kr>
2025-04-14 15:51:36 +0900

    feat: Access Token 발급을 위한 JwtUtils 생성

---
+++ src/main/java/com/moonbaar/common/utils/JwtUtils.java
@@ -0,0 +1,89 @@
+package com.moonbaar.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    private Key key;
+
+    private final long ACCESS_TOKEN_EXPIRATION = 1000L * 60 * 30; // 30분
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    // Access Token 생성
+    public String generateAccessToken(Long userId, String role) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + ACCESS_TOKEN_EXPIRATION);
+        return Jwts.builder()
+            .setSubject(String.valueOf(userId))
+            .claim("role", role)
+            .setIssuedAt(now)
+            .setExpiration(expiry)
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    // token에서 userId 추출
+    public Long getUserId(String token) {
+        return Long.valueOf(parseClaims(token).getSubject());
+    }
+
+    // token에서 role 추출
+    public String getRole(String token) {
+        return parseClaims(token).get("role", String.class);
+    }
+
+    // 토큰 유효성 검사
+    public boolean isValid(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (SecurityException | SignatureException e) {
+            log.error("서명 검증 실패 : {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("손상된 토큰 : {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 토큰 : {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("지원되지 않는 JWT : {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("빈 토큰 혹은 잘못된 값 : {}", e.getMessage());
+        } catch (JwtException e) {
+            log.error("기타 JWT 오류 : {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("예상치 못한 예외 : {}", e.getMessage());
+        }
+        return false;
+    }
+
+    // Claims 파싱
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+}
