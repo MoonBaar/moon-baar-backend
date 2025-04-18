c1f4d8d4a284f038bdf62c5e99f9da9e4f6cd902
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-18 20:31:06 +0900

    chore: request dto 필드 순서 변경

--- src/main/java/com/moonbaar/domain/visit/dto/FootprintRequest.java
+++ src/main/java/com/moonbaar/domain/visit/dto/FootprintRequest.java
@@ -7,11 +7,11 @@ public record FootprintRequest(
         @NotNull(message = "최소 위도는 필수 입력값입니다")
         BigDecimal minLat,
 
-        @NotNull(message = "최소 경도는 필수 입력값입니다")
-        BigDecimal minLng,
-
         @NotNull(message = "최대 위도는 필수 입력값입니다")
         BigDecimal maxLat,
+
+        @NotNull(message = "최소 경도는 필수 입력값입니다")
+        BigDecimal minLng,
 
         @NotNull(message = "최대 경도는 필수 입력값입니다")
         BigDecimal maxLng
