e7b1f439ae2d6724ae1923024995685e2b432c86
허서영 <hsy3130@outlook.kr>
2025-04-22 18:57:43 +0900

    feat: GET /users/me 엔드포인트 구현

--- src/main/java/com/moonbaar/domain/user/controller/UserController.java
+++ src/main/java/com/moonbaar/domain/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.moonbaar.domain.user.controller;
 
+import com.moonbaar.common.oauth.CustomUserDetails;
+import com.moonbaar.domain.user.dto.UserInfoResponse;
 import com.moonbaar.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +32,10 @@ public class UserController {
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
         userService.logout(request, response);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserInfoResponse> getLoginUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(UserInfoResponse.from(userDetails.getUser()));
     }
 }
