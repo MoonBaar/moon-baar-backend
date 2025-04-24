e14e643fad86dc0788bbe03d9a49ec7b5feab487
허서영 <hsy3130@outlook.kr>
2025-04-24 14:40:58 +0900

    refactor: userRepository에서 JPA 기본 제공 메서드 삭제
    - user 삭제 시 nativeQuery 대신 JPA가 제공하는 deleteById를 사용하도록 변경

--- src/main/java/com/moonbaar/domain/user/repository/UserRepository.java
+++ src/main/java/com/moonbaar/domain/user/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.moonbaar.domain.user.repository;
 
 import com.moonbaar.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,13 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByOauthIdAndOauthProvider(String oauthId, String oauthProvider);
-
-    Optional<User> findById(Long id);
-
-    boolean existsById(Long id);
-
-    @Modifying
-    @Query(value = "DELETE FROM users WHERE id = :id", nativeQuery = true)
-    void deleteById(Long id);
 
 }
