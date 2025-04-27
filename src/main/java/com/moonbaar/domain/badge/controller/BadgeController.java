527ecc799cfb8bd0654febc2d5237a4916532c9e
허서영 <hsy3130@outlook.kr>
2025-04-28 02:39:20 +0900

    feat: POST /users/me/badges/new 엔드포인트 구현

--- src/main/java/com/moonbaar/domain/badge/controller/BadgeController.java
+++ src/main/java/com/moonbaar/domain/badge/controller/BadgeController.java
@@ -23,4 +23,14 @@ public class BadgeController {
         return badgeService.findUserBadges(userDetails.getUser());
     }
 
+    /**
+     * 사용자가 새로운 배지를 얻을 수 있는지 확인하고, 새로 얻은 배지 목록을 조회한다.
+     * @param userDetails 로그인된 사용자
+     * @return 새로 얻은 배지 목록
+     */
+    @PostMapping("/new")
+    public UserBadgeListResponse checkAndGrantBadges(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return badgeService.checkAndGrantUserBadges(userDetails.getUser());
+    }
+
 }
--- src/main/java/com/moonbaar/domain/badge/repository/UserBadgeRepository.java
+++ src/main/java/com/moonbaar/domain/badge/repository/UserBadgeRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Set;
 
 public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
 
     @Query("SELECT ub FROM UserBadge ub JOIN FETCH ub.badge WHERE ub.user.id = :userId")
     List<UserBadge> findByUser(Long userId);
+
+    @Query("SELECT b.code FROM UserBadge ub JOIN ub.badge b WHERE ub.user.id = :userId")
+    Set<String> findCodeByUser(Long userId);
 
 }
