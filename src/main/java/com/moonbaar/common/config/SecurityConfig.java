1610ae9756da06355ca5b263423018f8c5b5a4e4
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-04-30 15:57:04 +0900

    feat: 프론트 배포 서버 allowed origins 추가

--- src/main/java/com/moonbaar/common/config/SecurityConfig.java
+++ src/main/java/com/moonbaar/common/config/SecurityConfig.java
@@ -62,7 +62,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://moonbaar.netlify.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
