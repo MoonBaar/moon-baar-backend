d0466696d810edae1d65167c4ab05cd9a9cc67fe
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-05-02 16:51:13 +0900

    feat: 방문 인증 시 진행 중인 행사 검증 추가

--- src/main/java/com/moonbaar/domain/visit/exception/VisitErrorCode.java
+++ src/main/java/com/moonbaar/domain/visit/exception/VisitErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum VisitErrorCode implements ErrorCode {
 
     RECENTLY_VISITED("V001", "재방문 가능 시간이 아닙니다.", HttpStatus.CONFLICT),
-    INVALID_LOCATION("V002", "유효하지 않은 위치입니다. 행사장과 너무 멉니다.", HttpStatus.FORBIDDEN);
+    INVALID_LOCATION("V002", "유효하지 않은 위치입니다. 행사장과 너무 멉니다.", HttpStatus.FORBIDDEN),
+    EVENT_NOT_ACTIVE("V003", "현재 진행 중인 행사가 아닙니다.", HttpStatus.FORBIDDEN),
+    ;
 
     private final String code;
     private final String message;
